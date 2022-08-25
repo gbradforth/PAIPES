540a2c7e521dc4455a02c8d19e7cab2b14421321
Gwen Bradforth <gbradfor@usc.edu>
2022-08-25 14:12:21 +0100

    init commit

---
+++ PAIPES/__init__.py
@@ -0,0 +1,52 @@
+import os
+
+from flask import Flask, render_template, send_file, abort
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'PAIPES.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/index')
+    def mockup():
+        return render_template('mockup.html')
+
+
+    @app.route('/', defaults={'req_path': ''})
+    @app.route('/<path:req_path>')
+    def dir_listing(req_path):
+        BASE_DIR = '/PAIPES/datasets'
+
+        # Joining the base and the requested path
+        abs_path = os.path.join(BASE_DIR, req_path)
+
+        # Return 404 if path doesn't exist
+        if not os.path.exists(abs_path):
+            return abort(404)
+
+        # Check if path is a file and serve
+        if os.path.isfile(abs_path):
+            return send_file(abs_path)
+
+        # Show directory contents
+        files = os.listdir(abs_path)
+        return render_template('base.html', files=files)
+
+    return app
